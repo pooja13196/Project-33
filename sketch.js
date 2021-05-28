e6940df119022dc50892a3ff05f59943e66e5bfb
pooja13196 <73737479+pooja13196@users.noreply.github.com>
2021-05-28 23:26:05 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,40 +1,37 @@
 const Engine = Matter.Engine;
 const World= Matter.World;
-const Bodies = Matter.Bodies;
+const Bodies= Matter.Bodies;
 
-var engine,world;
-var backgroundImg;
-var snowImg;
-var snow;
-var snowflake;
-var snowflakeImg;
+var snowflake,snowflakeImg
+var bgImg
 
 function preload(){
-backgroundImg=loadImage("snow3.jpg");
-snow=loadImage("snow4.webp");
-snowflake=loadImage("snow5.webp")
+  bg=loadImage("snow3.jpg");
+  snowflakeImg = loadImage("snow4.webp");
 
 }
 
 function setup() {
-  createCanvas(800,400);
-  engine=Engine.create();
-  world=engine.world;
-
-    } 
+  createCanvas(1000,600);
+  engine = Engine.create();
+  world = engine.world;
+}
 
 function draw() {
-  background(backgroundImg);
+  background(bg); 
   drawSprites();
+
+  //engine = Engine.update();
+  
   createSnowflake();
 }
 
 function createSnowflake(){
   if(frameCount%10===0){
-    snowflake=createSprite(random(0,900),(0,50,50));
-    snowflake.velocityX=-2
-    snowflake.velocityY=4;
-    snowflake.addImage(snowflakeImg);
-    snowflake.scale=0.1;
+  snowflake = createSprite(random(0,800), 0, 50, 50);
+  snowflake.velocityX = -2;
+  snowflake.velocityY = 4;
+  snowflake.addImage(snowflakeImg);
+  snowflake.scale = 0.1;
   }
 }
